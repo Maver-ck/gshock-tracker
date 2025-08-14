3b11763c080af4d4d4a2a5c83e68bf4aaed7706e
Yasser Ansari <yasser.ansari@hotmail.com>
2025-08-14 11:36:33 +0100

    Initial commit

---
+++ src/main/java/com/maverick/gshocktracker/GShockFitnessTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.maverick.gshocktracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GShockFitnessTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GShockFitnessTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maverick/gshocktracker/GShockFitnessTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maverick.gshocktracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GShockFitnessTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
